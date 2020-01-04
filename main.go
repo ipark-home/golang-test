f3957e75215e02af0eec902fa71111ab1351be75
iparkh <igor.y.parhomenko@gmail.com>
2020-01-04 12:16:43 +0200

    first

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Привіт")
+	// 1
+	// s, sep := "", ""
+	// for _, arg := range os.Args[1:] {
+	// 	s += sep + arg
+	// 	sep = " "
+	// }
+	// fmt.Println(s)
+	// 2
+	// fmt.Println(strings.Join(os.Args[1:], " "))
+
+}
